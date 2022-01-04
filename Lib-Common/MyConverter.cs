becd7d7ddff6b160e4b1a3fcd1bf4e1ed2b6d304
Adrian Vogt <adrian@myhandycash.com>
2022-01-04 16:06:12 +0100

    Sample Project to demonstrate the issue

---
+++ Lib-Common/MyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using SkiaSharp;
+using Svg.Skia;
+
+namespace Lib_Common
+{
+    public class MyConverter
+    {
+        public static byte[] SVGToPNG(byte[] svgData)
+        {
+            using (var svg = new SKSvg())
+            {
+                using (var inputStream = new MemoryStream(svgData))
+                {
+                    svg.Load(inputStream);
+                }
+
+                using (var outputStream = new MemoryStream())
+                {
+                    svg.Picture.ToImage(outputStream, SKColor.Empty, SKEncodedImageFormat.Png, 100, 1f, 1f, SKColorType.Bgra8888, SKAlphaType.Premul, SKColorSpace.CreateSrgb());
+
+                    return outputStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ svg.skia.bug.13634/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using Lib_Common;
+using System;
+using UIKit;
+
+namespace svg.skia.bug._13634
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            var svgData = new byte[] { 0, 2, 3, 4, 5, 0 }; // NOT a real SVG Document
+            MyConverter.SVGToPNG(svgData); // will crash with this input, but we are interested only in linker for this dummy project
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
